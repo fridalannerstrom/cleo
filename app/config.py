4f463151f7d9e52037032056b4a0712388699e0d
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-03-19 12:14:07 +0000

    fix: remove secret key that was not needed

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")  # För framtida autentisering
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # API-nyckeln för OpenAI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Läs in databasen URL från Render (eller använd en annan miljövariabel om du har den lokalt)
